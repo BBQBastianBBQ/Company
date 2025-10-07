0e0ae4a4bf18225fc6df1de301086290482c79e5
BBQBastian <BBQbastianBBQ@bbq.de>
2025-10-07 12:19:49 +0200

    -->den HTML Content, der im  IF-Statement stehen soll, wenn nur $_GET aktiv ist, in IF-Statement kopiert --> bei firstupdate.php <form> mit <div> umschlossen...

--- firstcreate.php
+++ firstcreate.php
@@ -1,38 +1,30 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    ?>
 
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $conn = new PDO('mysql:host=localhost;dbname=company', 'bstnremo', 'X1dl§eAA7');
-    $stmt = $conn->prepare("INSERT INTO employer(fname,lname) VALUES(:fname,:lname)");
-    $stmt->bindParam(':fname', $fname);
-    $stmt->bindParam(':lname', $lname);
-    $stmt->execute();
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="http://www.bastian.web.bbq/mystyle.css?v=<?php echo time(); ?>" type="text/css">
 
-} ?>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Title</title>
-        <link rel="stylesheet" href="http://www.bastian.web.bbq/mystyle.css?v=<?php echo time(); ?>" type="text/css">
+    <!-- WICHTIG: man muss für Firefox in die CSS Info eine Instanz einbauen, die dafür sorgt, dass der Browser
+     nicht die im Cache gespeicherte .CSS Datei nutzt, sondern immer die neuste Version lädt, sonst werden Veränderungen
+     nicht richtig angezeigt-->
 
-        <!-- WICHTIG: man muss für Firefox in die CSS Info eine Instanz einbauen, die dafür sorgt, dass der Browser
-         nicht die im Cache gespeicherte .CSS Datei nutzt, sondern immer die neuste Version lädt, sonst werden Veränderungen
-         nicht richtig angezeigt-->
-
-    </head>
+</head>
 <body class='body'>
 
 <h1> Neues Personal Registrieren</h1><br>
 
 <div class='form'>
-<form action='' method='post'>
-    <input type="text" name="fname"placeholder="fname">
-    <input type="text" name="lname"placeholder="lname">
-    <input type="submit" value="abschicken">
-</form>
+    <form action='' method='post'>
+        <input type="text" name="fname"placeholder="fname">
+        <input type="text" name="lname"placeholder="lname">
+        <input type="submit" value="abschicken">
+    </form>
 </div>
 <p><a href="../firstread.php">hhier geht es zur Tabelle</a></p>
 <br>
@@ -40,4 +32,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 
 </body>
 </html>
+<?Php
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $conn = new PDO('mysql:host=localhost;dbname=company', 'bstnremo', 'X1dl§eAA7');
+    $stmt = $conn->prepare("INSERT INTO employer(fname,lname) VALUES(:fname,:lname)"); // die bindParam-Methode wird initiiert
+    $stmt->bindParam(':fname', $fname); // es wird dafür gesorgt, dass keine SQL-Injektion o.Ä. stattfinden kann, da
+    $stmt->bindParam(':lname', $lname); // alles immer in einen SQL-String umgewandelt wird, was von FORM kommt
+    $stmt->execute();
+
+} ?>
+
 
--- firstupdate.php
+++ firstupdate.php
@@ -1,19 +1,7 @@
 <?php
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $id=$_GET['id'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $conn = new PDO('mysql:host=localhost;dbname=company', 'bstnremo', 'X1dl§eAA7');
-    $sql2 = 'UPDATE employer SET fname=:fname, lname=:lname WHERE id=:id';
-    $stmt = $conn->prepare($sql2);
-    $stmt->bindParam(':id',$id);
-    $stmt->bindParam(':fname', $fname);
-    $stmt->bindParam(':lname', $lname);
-    $stmt->execute();
-}
-?>
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -29,13 +17,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body class="body">
 
 <h1> Neues Personal Registrieren</h1><br>
+<div class="form">
 <form action='' method='post'>
     <input type="text" name="fname"placeholder="fname">
     <input type="text" name="lname"placeholder="lname">
     <input type="submit" value="abschicken">
 </form><br>
-
+</div>
 <h2><a href="../firstread.php">hier gehts zurück</a></h2>
 
 </body>
 </html>
+<?php
+}
+
+
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id=$_GET['id'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $conn = new PDO('mysql:host=localhost;dbname=company', 'bstnremo', 'X1dl§eAA7');
+    $sql2 = 'UPDATE employer SET fname=:fname, lname=:lname WHERE id=:id';
+    $stmt = $conn->prepare($sql2);
+    $stmt->bindParam(':id',$id);
+    $stmt->bindParam(':fname', $fname);
+    $stmt->bindParam(':lname', $lname);
+    $stmt->execute();
+}
+?>
