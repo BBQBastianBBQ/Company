3e265d4e9c9377402028f280d8028fa11ce11534
BBQBastian <BBQbastianBBQ@bbq.de>
2025-10-08 12:54:11 +0200

    COde aktualisiert und dafür gesorgt, dass das Formular bei lerrem Namesnfeld den bestehenden Namen beibehält. --> Kommentare eingefügt, die den Prozess erklären

--- update_department.php
+++ update_department.php
@@ -1,7 +1,25 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $id=$_GET['id'];
+    $conn = new PDO('mysql:host=localhost;dbname=company','bstnremo','X1dl§eAA7');
+    $sql='SELECT * FROM Department WHERE id=:id';
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id',$id);
+    $stmt->execute();
+    $result=$stmt->fetch(PDO::FETCH_ASSOC);
+
+    /* ich muss im Code berücksichtigen, dass ein Update auch ohne Änderung des Namens vollzogen werden könnte.
+       Für diesen Fall muss ich dafür sorgen, dass ich die aktuellen Werte in der Tabelle berücksichtige.
+       Diese Situation lässt sich dadurch lösen, dass ich ein Array ($result)erzeuge, in dem ich alle Werte, die ich im $sql
+       Statement anfordere speicher.
+       In "<form>" kann ich dann diese Werte als Default setzen und in das $_POST Array übertragen, wenn "submit" ausgelöst wird
+       und keine Namens-Aktualisierung stattfindet! */
+
+
+    var_dump($result);
     ?>
+
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -16,19 +34,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 
     <body class='body'>
 
+    <div class="bodydiv">
     <h1> DEPARTMENT UPDATEN</h1><br>
 
     <div class='form'>
         <form action='' method='post'>
-            <input type="text" name="name"placeholder="Department">
+            <input type="text" name="name" placeholder="<?php  echo $result['name']?>" value="<?php echo $result['name']?>"/>
 
+            <!-- die Werte aus dem erzeugten Array werden als default gesetzt -->
+
+            <p>Hiring? <input type="checkbox" name="yes" value="1" /></p>
             <input type="submit" value="abschicken">
         </form>
     </div>
+    <div class="lynx">
     <p><a href="../read_department.php">hier geht es zur Tabelle</a></p>
-    <br>
+    </div>
+    <div class="lynx">
     <p><a href="http://www.bastian.web.bbq/index.php">hier gehts zurück zu meiner Seite</a></p>
-
+    </div>
+    </div>
     </body>
     </html>
     <?php
@@ -38,12 +63,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id=$_GET['id'];
     $name = $_POST['name'];
+    $hire = $_POST['yes'] ?? 0 ;
 
     $conn = new PDO('mysql:host=localhost;dbname=company', 'bstnremo', 'X1dl§eAA7');
-    $sql2 = 'UPDATE Department SET name=:name  WHERE id=:id';
+    $sql2 = 'UPDATE Department SET name=:name,hiring=:hiring  WHERE id=:id';
     $stmt = $conn->prepare($sql2);
     $stmt->bindParam(':id',$id);
     $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':hiring', $hire);
 
     $stmt->execute();
     ?>
